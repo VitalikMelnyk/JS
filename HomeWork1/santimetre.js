5d99923017cb91aae04ec2535a8db8516dd313be
unknown <melnyk.vitalikk@gmail.com>
2019-01-29 18:16:54 +0200

    Updating translating

--- HomeWork1/santimetre.js
+++ HomeWork1/santimetre.js
@@ -7,7 +7,7 @@
 // var number = prompt("Input number in metre: ",' ');
 // alert(translate()); 
 function translate(){
-	return number * 1000;
+	return number / 1000;
 }
 var number = prompt("Input number in metre: ",' ');
 alert(translate());
